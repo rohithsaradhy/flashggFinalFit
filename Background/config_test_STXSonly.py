f0e906064550ca69f38772a5957f3c8c1c931fae
rohithsaradhy <rohithsaradhy@gmail.com>
2022-08-26 11:52:21 +0200

    Added Configuration for STXS/ANOM vs STXSOnly

--- Background/config_test_STXSonly.py
+++ Background/config_test_STXSonly.py
@@ -3,10 +3,10 @@
 backgroundScriptCfg = {
   
   # Setup
-  'inputWSDir':'/eos/user/r/rsaradhy/vh_anomalous/2022_08_22_STXS_ANOM_Cuts/workspaces', # location of 'allData.root' file
-  'cats':'WHLeptonicTag_0,WHLeptonicTag_1,WHLeptonicTag_2,WHLeptonicTag_3,WHLeptonicTag_4', # auto: automatically inferred from input ws
+  'inputWSDir':'/eos/user/r/rsaradhy/vh_anomalous/2022_08_23_STXS_Cuts/workspaces', # location of 'allData.root' file
+  'cats':'WHLeptonicTag_0,WHLeptonicTag_1,WHLeptonicTag_2', # auto: automatically inferred from input ws
   'catOffset':0, # add offset to category numbers (useful for categories from different allData.root files)  
-  'ext':'2022_08_22_STXS_ANOM', # extension to add to output directory
+  'ext':'2022_08_23_STXS', # extension to add to output directory
   'year':'2018', # Use combined when merging all years in category (for plots)
 
   # Job submission options
